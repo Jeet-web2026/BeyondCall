44dbb4409deeb60ba86cf392923b6f78a7b8e025
Jeet <150388284+Jeet-web2026@users.noreply.github.com>
2024-07-03 18:39:29 +0000

    j

--- BeyondCall/routes/web.php
+++ BeyondCall/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\Controller;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register', function () {
+    return view('hello');
 });
