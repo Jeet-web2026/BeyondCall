9fcaae6e17d729a3e6bd976a6412358e43dc14a7
Jeet <150388284+Jeet-web2026@users.noreply.github.com>
2024-07-04 18:03:51 +0000

    j

--- BeyondCall/app/Http/Controllers/userController.php
+++ BeyondCall/app/Http/Controllers/userController.php
@@ -6,10 +6,11 @@ use App\Models\User;
 
 abstract class Controller
 {
-    public function register($request){
+    public function register(run $request)
+    {
         $data =  $request->validate([
-            "email"=> "required|email",
-            "password"=> "required|password",
+            "email" => "required|email",
+            "password" => "required|password",
         ]);
 
         $user = User::create($data);
